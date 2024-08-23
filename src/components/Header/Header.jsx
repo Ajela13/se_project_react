b93c1b9302bdc78b98c53d99a346d801b0d050bd
alejandra marino <alejamarino123@gmail.com>
2024-08-23 15:32:00 -0400

    feat: add Header and Header styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+function Header() {
+  return (
+    <div className="header">
+      <img className="header__logo" src={logo} alt="logo" />
+      <p className="header__date-and-location">date and location</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user">
+        <p className="header__username">Terrence Tegegne</p>
+        <img
+          src={avatar}
+          alt="Terrence Tegegne avatar"
+          className="header__avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
