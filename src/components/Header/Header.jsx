a7e75b6dcdadfaae8d53dfaa7774c20a45ca717c
alejandra marino <alejamarino123@gmail.com>
2024-08-23 20:14:59 -0400

    fix: add currentDate to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,15 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <div className="header">
       <img className="header__logo" src={logo} alt="logo" />
-      <p className="header__date-and-location">date and location</p>
+      <p className="header__date-and-location">{currentDate} and location</p>
       <button className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user">
         <p className="header__username">Terrence Tegegne</p>
