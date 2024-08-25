a47339c5349818614fa99009ac945f9b9ac0283e
alejandra marino <alejamarino123@gmail.com>
2024-08-25 18:41:37 -0400

    feat: add modal with form "new clothes"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
@@ -18,6 +19,47 @@ function App() {
           <Main weatherData={weatherData} />
           <Footer />
         </div>
+        <ModalWithForm title="New garment" buttonText="Add garment">
+          <label htmlFor="name" className="modal__label">
+            Name
+            <input type="text" className="modal__input" placeholder="Name" />
+          </label>
+
+          <label htmlFor="imageUrl" className="modal__label">
+            Image
+            <input
+              type="url"
+              className="modal__input"
+              id="imageUrl"
+              placeholder="Image URL"
+            />
+          </label>
+
+          <fieldset className="modal__radio-btns">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="hot" type="radio" className="modal__radio-input" />
+              Hot
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="warm" type="radio" className="modal__radio-input" />
+              Warm
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="cold" type="radio" className="modal__radio-input" />
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
       </div>
     </>
   );
