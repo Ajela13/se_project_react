030e8efc0cad633fae0f749774e0a4a2506a7646
alejandra marino <alejamarino123@gmail.com>
2024-08-24 21:35:41 -0400

    feat: add footer content

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__copyrigth">Developed by Alejandra Marino</p>
+      <p className="footer__production-year">2024</p>
+    </div>
+  );
+}
+
+export default Footer;
