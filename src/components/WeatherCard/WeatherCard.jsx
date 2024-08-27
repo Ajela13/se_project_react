6153463eacbe252f874d591e47550a0f680f4ea3
alejandra marino <alejamarino123@gmail.com>
2024-08-26 20:58:10 -0400

    feat: add images functionality, change depending on the day and condition

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,12 +1,27 @@
 import "./WeatherCard.css";
-import sunny from "../../assets/day/clear.svg";
+import { weatherOptions } from "../../utils/constants";
+
 function WeatherCard({ weatherData }) {
+  const filterOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+  const weatherOption = filterOptions[0];
+
   return (
     <div className="weatherCard">
       <p className="weatherCard__temperature">
         {weatherData.temperature} &deg; F
       </p>
-      <img className="weatherCard__image" src={sunny} alt="weather image" />
+      <img
+        className="weatherCard__image"
+        src={weatherOption?.url}
+        alt={`${weatherOptions?.day ? "day" : "night"} time ${
+          weatherOption?.condition
+        }`}
+      />
     </div>
   );
 }
