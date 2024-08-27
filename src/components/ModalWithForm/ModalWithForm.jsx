257b4dce20fbb89552d14eeca920032ce23e2933
alejandra marino <alejamarino123@gmail.com>
2024-08-27 12:09:49 -0400

    fix: delete empty tag

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -7,26 +7,22 @@ function ModalWithFrom({
   handleCloseClick,
 }) {
   return (
-    <>
-      <div
-        className={`modal ${activeModal === "add-clothes" && "modal_opened"}`}
-      >
-        <div className="modal__content">
-          <h2 className="modal__title">{title}</h2>
-          <button
-            onClick={handleCloseClick}
-            type="button"
-            className="modal__close"
-          ></button>
-          <form action="" className="modal__form">
-            {children}
-            <button type="submit" className="modal__submit">
-              {buttonText}
-            </button>
-          </form>
-        </div>
+    <div className={`modal ${activeModal === "add-clothes" && "modal_opened"}`}>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button
+          onClick={handleCloseClick}
+          type="button"
+          className="modal__close"
+        ></button>
+        <form action="" className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
 
