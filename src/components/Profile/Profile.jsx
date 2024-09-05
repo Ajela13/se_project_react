e3405c218a2512c74ab9b636b900a74cd3577cc0
alejandra marino <alejamarino123@gmail.com>
2024-09-05 15:12:58 -0400

    feat: add profile component with sub components

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,7 @@
+import "./Profile.css";
+import SideBar from "../SideBar/SideBar";
+import ClothesSection from "../ClothesSection/ClothesSection";
+
+function Profile() {}
+
+export default Profile;
