91fc63920a329d1a4797792d6ed428b6a2ca0e40
alejandra marino <alejamarino123@gmail.com>
2024-09-05 16:33:23 -0400

    feat: add clothesSection

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,5 +1,27 @@
 import "./ClothesSection.css";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function ClothesSection() {}
+function ClothesSection() {
+  return (
+    <div className="clothesSection">
+      <div className="clothesSection__options">
+        <p className="clothesSection__title">Your items</p>
+        <button className="clothesSection__add-item">+ Add new</button>
+      </div>
+      <ul className="clothesSection__list">
+        {defaultClothingItems.map((item) => {
+          return (
+            <ItemCard
+              key={item._id}
+              item={item}
+              //   onCardClick={handleCardClick}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default ClothesSection;
