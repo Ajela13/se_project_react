f2a8dc813fc57c7960a82320f5a10609dcb5f932
alejandra marino <alejamarino123@gmail.com>
2024-09-09 15:18:40 -0400

    refacror: add modal component and update props

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,21 +1,18 @@
 import "./ModalWithForm.css";
+import Modal from "../Modal/Modal";
 function ModalWithForm({
   children,
   buttonText,
   title,
   isOpen,
-  handleCloseClick,
+  onClose,
   onSubmit,
 }) {
   return (
-    <div className={`modal ${isOpen && "modal_opened"}`}>
+    <Modal name="form" onClose={onClose} isOpen={isOpen}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button
-          onClick={handleCloseClick}
-          type="button"
-          className="modal__close"
-        />
+        <button onClick={onClose} type="button" className="modal__close" />
         <form onSubmit={onSubmit} className="modal__form">
           {children}
           <button type="submit" className="modal__submit">
@@ -23,7 +20,7 @@ function ModalWithForm({
           </button>
         </form>
       </div>
-    </div>
+    </Modal>
   );
 }
 
