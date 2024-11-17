46785ef0ca2a2bb38fb6d68457ff970d7205f8b3
alejandra marino <alejamarino123@gmail.com>
2024-11-17 17:42:27 -0500

    register and authorization api req

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+export const BASE_URL = "http://localhost:3001";
+
+export const register = (name, avatar, email, password) => {
+  return fetch(`${BASE_URL}/auth/local/register`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/auth/local`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
