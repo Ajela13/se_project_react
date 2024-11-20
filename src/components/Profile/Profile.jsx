67a11af2285205e528fd3d26233d83e42015efa2
alejandra marino <alejamarino123@gmail.com>
2024-11-20 14:17:54 -0500

    add handler update profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,10 +2,15 @@ import "./Profile.css";
 import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 
-function Profile({ handleCardClick, clothingItems, handleAddClick }) {
+function Profile({
+  handleCardClick,
+  clothingItems,
+  handleAddClick,
+  handleUpdateProfileClick,
+}) {
   return (
     <div className="profile">
-      <SideBar />
+      <SideBar handleUpdateProfileClick={handleUpdateProfileClick} />
       <ClothesSection
         handleCardClick={handleCardClick}
         handleAddClick={handleAddClick}
