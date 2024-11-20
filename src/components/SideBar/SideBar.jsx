c88d319a2f99484c69b88ad0e153cdf76a0bc071
alejandra marino <alejamarino123@gmail.com>
2024-11-20 12:23:06 -0500

    add profile update and logOut button

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,22 @@
 import "./SideBar.css";
-import avatar from "../../assets/avatar.png";
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function SideBar() {
+  const { currentUser } = useContext(CurrentUserContext);
+
   return (
     <div className="sideBar">
-      <img
-        src={avatar}
-        alt="Terrence Tegegne avatar"
-        className="sideBar__avatar"
-      />
-      <p className="sideBar__username">Terrence Tegegne</p>
+      <div className="sideBar__user-info">
+        <img
+          src={currentUser.avatar}
+          alt="Terrence Tegegne avatar"
+          className="sideBar__avatar"
+        />
+        <p className="sideBar__username">{currentUser.name}</p>
+      </div>
+      <button className="sideBar__update-profile">Change profile data</button>
+      <button className="sideBar__LogOut">Log out</button>
     </div>
   );
 }
