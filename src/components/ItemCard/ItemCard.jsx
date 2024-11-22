1316c7de6e342a0472d7f47312266e768544bbcd
alejandra marino <alejamarino123@gmail.com>
2024-11-22 13:55:15 -0500

    add handleLike and like className logic

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,16 +2,28 @@ import "./ItemCard.css";
 import { useContext } from "react";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-function ItemCard({ item, onCardClick }) {
+function ItemCard({ item, onCardClick, onCardLike }) {
+  const { currentUser } = useContext(CurrentUserContext);
+  const isOwn = item?.owner === currentUser._id;
+  const isLiked = item.likes.includes(currentUser._id);
+  const cardLikeButtonName = isLiked
+    ? "card__like-btn-active"
+    : "card__like-btn";
+
   const handleCardClick = () => {
     onCardClick(item);
   };
+  const handleLike = () => {
+    onCardLike({ id: item._id, isLiked });
+  };
 
-  const { currentUser } = useContext(CurrentUserContext);
-  const isOwn = item?.owner === currentUser._id;
   return (
     <li className="card" hidden={!isOwn}>
-      <h2 className="card__name">{item.name}</h2>
+      <div className="card__header">
+        <h2 className="card__name">{item.name}</h2>
+        <button className={cardLikeButtonName} onClick={handleLike}></button>
+      </div>
+
       <img
         onClick={handleCardClick}
         className="card__image"
