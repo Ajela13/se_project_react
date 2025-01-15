f798db238fcb6a0aae2d9d5819f2d1fc9accd985
alejandra marino <alejamarino123@gmail.com>
2025-01-14 23:44:36 -0500

    update baseUrl

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl = "https://api.wtwr.casepractice.com";
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
